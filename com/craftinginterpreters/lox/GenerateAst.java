f16517c4da292c9e928fdc0d7be8a472a7d73599
“leontan29” <leontann29@gmail.com>
2024-02-20 11:43:25 -0800

    save

---
+++ com/craftinginterpreters/lox/GenerateAst.java
@@ -0,0 +1,10 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    
+}
