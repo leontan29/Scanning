58b1d44e175a1a4c5d392515a1ddbb36ac08f0c0
leontan29 <leontann29@gmail.com>
2024-04-16 10:44:12 -0700

    buggy

--- com/craftinginterpreters/lox/Stmt.java
+++ com/craftinginterpreters/lox/Stmt.java
@@ -7,8 +7,23 @@ abstract class Stmt {
   abstract <R> R accept(Visitor<R> visitor);
 
   interface Visitor<R> {
+    R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
     R visitPrintStmt(Print stmt);
+    R visitVarStmt(Var stmt);
+  }
+
+  static class Block extends Stmt {
+    Block(List<Stmt> statements) {
+      this.statements = statements;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBlockStmt(this);
+    }
+
+    final List<Stmt> statements;
   }
 
   static class Expression extends Stmt {
@@ -35,6 +50,21 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+
+  static class Var extends Stmt {
+    Var(Token name, Expr initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVarStmt(this);
+    }
+
+    final Token name;
+    final Expr initializer;
   }
 
 }
