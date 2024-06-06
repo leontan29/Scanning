2de2766b15f001c3f034cd6c162133f380fe53bb
leontan29 <leontann29@gmail.com>
2024-06-06 15:33:01 -0700

    save

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -41,7 +41,8 @@ class Parser {
   
     private Stmt statement() {
 	if (match(PRINT)) return printStatement();
-
+	if (match(LEFT_BRACE)) return new Stmt.Block(block());
+	
 	return expressionStatement();
     }
    
